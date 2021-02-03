461d5791a0a6e4597371f22601c83fb87883d7c9
ahmed <repotecing@gmail.com>
2021-02-03 04:44:28 +0200

    first commit

---
+++ src/test/java/com/amd/micro/EurekraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amd.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
